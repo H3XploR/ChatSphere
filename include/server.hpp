060bb5960fc7283cb4e766335fc6f5b635cfa80c
null <hexplor@hexplor.com>
2025-08-29 00:09:25 +0200

    je produit du code pour server

--- include/server.hpp
+++ include/server.hpp
@@ -9,16 +9,22 @@
 #include <exception>
 #include <unistd.h>
 #include <netinet/in.h>
-
+#include <sys/epoll.h>
+#define EPOLL_QUEUE_LEN  10
+#define MAX_EPOLL_EVENTS_PER_RUN 10
+#define EPOLL_RUN_TIMEOUT 1000 // 1000 ms (1 second) is a common default
 class client;
 class channel;
 class Server {
     public:
-        int                 _fdSocket;
-        int                 _port;
-        std::vector<channel> _channel;
-        pollfd              _servPoll;
-        sockaddr_in         _serverAddress;
+        int                       _fdSocket;
+        int                       _port;
+        std::vector<channel>      _channel;
+        pollfd                    _servPoll;
+        sockaddr_in               _serverAddress;
+        int                       _epfd;
+    struct epoll_event _ev[MAX_EPOLL_EVENTS_PER_RUN];
+        int                       _client_sock;
         void                createNewChannel(std::string channelName);
 
         Server();
--- src/server.cpp
+++ src/server.cpp
@@ -17,12 +17,39 @@ Server::Server() : _fdSocket(socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0)), _
     // sending data
     std::string message = "Hello, server!";
     send(_fdSocket, message.c_str(), message.size(), 0);
+    //Create specific file descriptor for epoll calls
+    _epfd = epoll_create(EPOLL_QUEUE_LEN);
+  // Initialisation du tableau d'epoll_event pour chaque instance
+  for (int i = 0; i < MAX_EPOLL_EVENTS_PER_RUN; ++i) {
+    _ev[i].events = 0;
+    _ev[i].data.fd = -1;
+  }
+
+  // Ajout du client socket à epoll
+  struct epoll_event ev;
+  ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP;
+  ev.data.fd = _client_sock;
+  int res = epoll_ctl(_epfd, EPOLL_CTL_ADD, _client_sock, &ev);
+
+  while (1) {
+    // Attente d'événements sur les sockets
+    int nfds = epoll_wait(_epfd, _ev, MAX_EPOLL_EVENTS_PER_RUN, EPOLL_RUN_TIMEOUT);
+    if (nfds < 0)
+      std::cout << "Error in epoll_wait!" << std::endl;
+
+    // Pour chaque socket prêt
+    for (int i = 0; i < nfds; i++) {
+      int fd = _ev[i].data.fd;
+      handle_io_on_socket(fd);
+    }
+  }
 }
 
 Server::~Server() {
     if (_fdSocket >= 0) {
         close(_fdSocket);
         std::cout << "Socket " << _fdSocket << " closed.\n";
+        close(_epfd);
     }
 }
 
